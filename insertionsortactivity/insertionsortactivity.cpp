e8933d57760ba350cdcafcb19d32f503375bcfcb
falskuyyy <ahmdfalah10@gmail.com>
2023-03-23 19:11:41 +0700

    Membuat kondisi n tidak lebih dari 20

--- insertionsortactivity/insertionsortactivity.cpp
+++ insertionsortactivity/insertionsortactivity.cpp
@@ -4,11 +4,28 @@ using namespace std;
 int arr[20]			// Membuat array dengan panjang data 20
 int n;				// Membuat variable inputan n
 
-voit input()int
+void input()
 {	// Procedure Input
 	while (true)
 	{
 		cout << "Masukkan jumlah data pada Array : ";		// Membuat inputan jumlah element array
 		cin >> n;											// Memanggil variabel inputan n
+	
+		if (n <= 20)
+		{	// Membuat kondisi n tidak lebih dari 20
+			break;
+
+		}
+		else
+		{
+			cout << "\nArray yang anda Masukkan maksimal 20 elemen. \n";
+		}
 	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan element array" << endl;
+	cout << "======================" << endl;
+
+	// Menggunakan perulangan
+
 }

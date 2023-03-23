25da07484aec1edc0419fe25d89ebf94a99a2c7e
falskuyyy <ahmdfalah10@gmail.com>
2023-03-23 18:53:26 +0700

    Membuat inputan jumlah element array

--- insertionsortactivity/insertionsortactivity.cpp
+++ insertionsortactivity/insertionsortactivity.cpp
@@ -5,3 +5,10 @@ int arr[20]			// Membuat array dengan panjang data 20
 int n;				// Membuat variable inputan n
 
 voit input()int
+{	// Procedure Input
+	while (true)
+	{
+		cout << "Masukkan jumlah data pada Array : ";		// Membuat inputan jumlah element array
+		cin >> n;											// Memanggil variabel inputan n
+	}
+}

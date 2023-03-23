2075725cd4ee9dc454d0948c3c2d8d7d6e9af941
falskuyyy <ahmdfalah10@gmail.com>
2023-03-23 19:31:33 +0700

    3. setting nilai j sama dengan i-j;

--- insertionsortactivity/insertionsortactivity.cpp
+++ insertionsortactivity/insertionsortactivity.cpp
@@ -46,7 +46,7 @@ void insertionsort()
 	{
 		temp = arr[i];		// 2. simpan nilai arr[i} ke variable sementara temp
 
-		j = i - 1;		// 3. setting nilai j sama dengan i-j;
+		j = i - 1;		// 3. setting nilai j sama dengan i-j; 
 
 		while (j >= 0 && arr[j] > temp)	// 4. looping while dimana nilai j lebih besar
 	}
